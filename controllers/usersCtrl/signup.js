00f52cd9b09f7956d0f7e3051c2a8782b5fb680b
Andrey5353 <andreyae0117ab@gmail.com>
2023-03-12 05:14:05 +0200

    hw06-email

--- controllers/usersCtrl/signup.js
+++ controllers/usersCtrl/signup.js
@@ -1,6 +1,8 @@
 const { User } = require("../../models/userModel");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { uid } = require("uid");
+const { sendEmail } = require("../../helpers/sendgridEmail");
 
 const signup = async (req, res, next) => {
   try {
@@ -13,17 +15,35 @@ const signup = async (req, res, next) => {
       });
     }
 
+    const verificationToken = uid();
     const avatarURL = gravatar.url(email);
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    await User.create({ name, email, password: hashPassword, avatarURL });
+    await User.create({
+      name,
+      email,
+      password: hashPassword,
+      avatarURL,
+      verificationToken,
+    });
+
+    const mail = {
+      to: email,
+      subject: "Email submission",
+      html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Submit email</a>`,
+    };
+
+    await sendEmail(mail);
+
     res.status(201).json({
       status: "created",
       code: 201,
+      message: "Registration successful and verification email sent",
       data: {
         user: {
           email: `${email}`,
           subscription: "starter",
           avatarURL,
+          verificationToken,
         },
       },
     });
